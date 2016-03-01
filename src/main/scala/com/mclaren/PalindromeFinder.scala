c70f757e1da01e3f7788dbf216598ad6c98ce327
Alex Salguero <hasumedic@gmail.com>
2016-03-01 18:37:37 +0000

    Added simple scenarios of empty and single palindrome

--- src/main/scala/com/mclaren/PalindromeFinder.scala
+++ src/main/scala/com/mclaren/PalindromeFinder.scala
@@ -3,5 +3,8 @@ package com.mclaren
 case class PalindromeMatch(palindrome: String, index: Int)
 
 object PalindromeFinder {
-  def find(input: String): List[PalindromeMatch] = ???
+  def find(input: String): List[PalindromeMatch] = {
+    if (input.isEmpty) List()
+    else List(PalindromeMatch(input, 1))
+  }
 }
--- src/test/scala/PalindromeFinderSpec.scala
+++ src/test/scala/PalindromeFinderSpec.scala
@@ -1,8 +1,13 @@
-import com.mclaren.PalindromeFinder
+import com.mclaren.{PalindromeMatch, PalindromeFinder}
 import org.scalatest._
 
 class PalindromeFinderSpec extends FlatSpec with Matchers {
-  "PalindomeFinder" should "find no palindromes in an empty string" in {
+  "PalindromeFinder" should "find no palindromes in an empty string" in {
     PalindromeFinder.find("") should be (List())
+  }
+
+  "PalindromeFinder" should "find a palindrome when the whole word is" in {
+    val expected = PalindromeMatch("lol", 1)
+    PalindromeFinder.find("lol") should be (List(expected))
   }
 }

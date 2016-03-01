d392755a6ec970c33753158926fbf12c9d0794fb
Alex Salguero <hasumedic@gmail.com>
2016-03-01 19:34:29 +0000

    First generalization of a possible complete solution

--- src/main/scala/com/mclaren/PalindromeFinder.scala
+++ src/main/scala/com/mclaren/PalindromeFinder.scala
@@ -1,16 +1,25 @@
 package com.mclaren
 
+import scala.collection.immutable.IndexedSeq
+
 case class PalindromeMatch(palindrome: String, index: Int)
 
 object PalindromeFinder {
 
-  def isPalindrome(input: String): Boolean = {
-    if (input.isEmpty) false
+  private def isPalindrome(input: String): Boolean = {
+    if (input.length < 2) false
     else input == input.reverse
   }
 
-  def find(input: String): List[PalindromeMatch] = {
-    if (isPalindrome(input)) List(PalindromeMatch(input, 1))
-    else List()
+  private def findAllPalindromes(input: String): IndexedSeq[PalindromeMatch] = {
+    for {
+      start <- input.indices
+      end <- start to input.length
+      if isPalindrome(input.substring(start, end))
+    } yield PalindromeMatch(input.substring(start, end), start)
+  }
+
+  def find(input: String): IndexedSeq[PalindromeMatch] = {
+    findAllPalindromes(input)
   }
 }
--- src/test/scala/PalindromeFinderSpec.scala
+++ src/test/scala/PalindromeFinderSpec.scala
@@ -3,15 +3,20 @@ import org.scalatest._
 
 class PalindromeFinderSpec extends FlatSpec with Matchers {
   "PalindromeFinder" should "find no palindromes in an empty string" in {
-    PalindromeFinder.find("") should be (List())
+    PalindromeFinder.find("") should be(List())
   }
 
   it should "find a palindrome when the whole word is" in {
-    val expected = PalindromeMatch("lol", 1)
-    PalindromeFinder.find("lol") should be (List(expected))
+    val expected = PalindromeMatch("lol", 0)
+    PalindromeFinder.find("lol") should be(List(expected))
   }
 
   it should "find no palindromes when the are none in the input string" in {
-    PalindromeFinder.find("boo") should be (List())
+    PalindromeFinder.find("bov") should be(List())
+  }
+
+  it should "find a palindrome within the input string" in {
+    val expected = PalindromeMatch("lol", 2)
+    PalindromeFinder.find("zxlolcv") should be(List(expected))
   }
 }
